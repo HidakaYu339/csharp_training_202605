0b20a8c8571de664fdd576f5b7294789d6d5446c
HidakaYu339 <yumaitake@icloud.com>
2026-05-21 15:26:50 +0900

    リスト7-3 Repositoryインターフェイス

---
+++ EmpManageSystem/Applications/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+using EmpManageSystem.Applications.Domains;
+namespace EmpManageSystem.Applications.Repositories;
+/// <summary>
+/// ドメインオブジェクト:部門のCRUD操作インターフェイス
+/// </summary>
+public interface IDepartmentRepository
+{
+    /// <summary>
+    /// すべての部門を取得する
+    /// </summary>
+    /// <returns>部門のリスト</returns>
+    List<Department> FindAll();
+
+    /// <summary>
+    /// 指定された部門Idの部門を取得する
+    /// </summary>
+    /// <param name="id">部署Id</param>
+    /// <returns>取得して部署</returns>
+    Department? FindById(int id);
+}
