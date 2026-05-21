a981761af63b4f2a9df064849747fc63b28e414a
HidakaYu339 <yumaitake@icloud.com>
2026-05-21 15:33:24 +0900

    リスト8-1 Entityクラス

---
+++ EmpManageSystem/Infrastructures/Entities/DepartmentEntity.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EmpManageSystem.Infrastructures.Entities;
+/// <summary>
+/// 部門テーブル(department)を扱うEntity Framework Coreのエンティティクラス
+/// </summary>
+[Table("department")]
+public class DepartmentEntity
+{
+    /// <summary>
+    /// 部門Id(主キー)
+    /// </summary> 
+    [Key]
+    [Column("id")]
+    public int DeptId { get; set; }
+    /// <summary>
+    /// 部門名
+    /// </summary> 
+    [Column("name")]
+    public string DeptName { get; set; } = string.Empty;
+}
---
+++ EmpManageSystem/Infrastructures/Entities/EmployeeEntity.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EmpManageSystem.Infrastructures.Entities;
+/// <summary>
+/// 社員テーブル(employee)を扱うEntity Framework Coreのエンティティクラス
+/// </summary>
+[Table("employee")]
+public class EmployeeEntity
+{
+    /// <summary>
+    /// 社員Id(主キー)
+    /// </summary>
+    [Key]
+    [Column("id")]
+    public int EmpId { get; set; }
+    [Column("name")]
+    /// <summary>
+    /// 社員名
+    /// </summary>
+    public string EmpName { get; set; } = string.Empty;
+    /// <summary>
+    /// 所属部門Id(外部キー)
+    /// </summary>
+    [Column("dept_id")]
+    public int? DeptId { get; set; }
+}
